1c66dbb52b9af47640210d1a0b0b5a368a135af2
wanxuhao <836872513@qq.com>
2026-06-06 20:03:51 +0800

    提交07

---
+++ SAA-07StructuredOutput/src/test/java/com/atguigu/study/Saa07StructuredOutputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Saa07StructuredOutputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
